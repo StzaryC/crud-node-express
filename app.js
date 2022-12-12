415ce9d5bfc992fcd05a898e1abf1683a2fa4730
Gustavo Park <80788755+StzaryC@users.noreply.github.com>
2022-12-12 00:00:55 -0600

    updated env password sintax error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     database: process.env.DATABASE,
     user: process.env.USER,
-    password: PROCES.env.PASSWORD,
+    password: process.env.PASSWORD,
 })
 connection.connect((err)=>{
     if(err){
